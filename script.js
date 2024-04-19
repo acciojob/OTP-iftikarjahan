3a66e44c32301a9c0a26de92abc5e92016968622
iftikarjahan <69801895+iftikarjahan@users.noreply.github.com>
2024-04-19 17:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll(".otp");
+inputs.forEach((input, idx) => {
+    input.addEventListener("keyup", () => {
+        if (input.value && idx < inputs.length - 1) {
+            inputs[idx + 1].focus();
+        }
+    });
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && idx > 0) {
+            inputs[idx - 1].focus();
+        }
+    });
+});
